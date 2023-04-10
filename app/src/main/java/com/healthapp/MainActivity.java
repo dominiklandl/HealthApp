8d1ac0f13139c79deece28f16bd052b7b73790ec
emilmitt <129877903+emilmitt@users.noreply.github.com>
2023-04-10 12:08:22 +0200

    restliche launch Methoden hinzugefügt + Activity labels hinzugefügt

--- app/src/main/java/com/healthapp/MainActivity.java
+++ app/src/main/java/com/healthapp/MainActivity.java
@@ -12,10 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Home");
     }
 
     public void launchSettings(View v){
         Intent i = new Intent(this, SettingsActivity.class);
         startActivity(i);
+    }
+
+    public void launchPuls(View v){
+        Intent i1 = new Intent(this, PulsActivity.class);
+        startActivity(i1);
+    }
+
+    public void launchBlutdruck(View v){
+        Intent i2 = new Intent(this, BlutdruckActivity.class);
+        startActivity(i2);
+    }
+
+    public void launchTemp(View v){
+        Intent i3 = new Intent(this, TemperaturActivity.class);
+        startActivity(i3);
     }
 }
