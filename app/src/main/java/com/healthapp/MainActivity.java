a109638bf5e51ce5a7d9bb6fc1d57cb95883a3f3
emilmitt <129877903+emilmitt@users.noreply.github.com>
2023-04-19 01:09:54 +0200

    bottom navigation bar + home/profile/settings fragments added

--- app/src/main/java/com/healthapp/MainActivity.java
+++ app/src/main/java/com/healthapp/MainActivity.java
@@ -1,23 +1,62 @@
 package com.healthapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.healthapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
         setTitle("Home");
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    getSupportActionBar().setTitle("Home");
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    getSupportActionBar().setTitle("Profile");
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    getSupportActionBar().setTitle("Settings");
+                    break;
+
+            }
+
+
+            return true;
+        });
     }
 
-    public void launchSettings(View v){
-        Intent i = new Intent(this, SettingsActivity.class);
-        startActivity(i);
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+
+
     }
 
     public void launchPuls(View v){
@@ -34,4 +73,6 @@ public class MainActivity extends AppCompatActivity {
         Intent i3 = new Intent(this, TemperaturActivity.class);
         startActivity(i3);
     }
+
+
 }
