5f4ac6f8513b586b224bf0e25b864f68cd5eb610
tobiasschauer <129883147+tobiasschauer@users.noreply.github.com>
2023-05-12 17:23:50 +0200

    Bearbeiten des Blutdruckfensters: Werte können jetzt eingeben werden -> wird der Button gedrückt, so werden die Werte im TextView ausgeben

--- app/src/main/java/com/healthapp/BlutdruckActivity.java
+++ app/src/main/java/com/healthapp/BlutdruckActivity.java
@@ -3,12 +3,32 @@ package com.healthapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BlutdruckActivity extends AppCompatActivity {
+
+
+    EditText Blutdrucks;
+    EditText Blutdruckd;
+    TextView Textfieldblutdrucks;
+    TextView Textfieldblutdruckd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blutdruck);
+        Textfieldblutdrucks = (TextView) findViewById(R.id.Textfieldblutdrucks);
+        Textfieldblutdruckd = (TextView) findViewById(R.id.Textfieldblutdruckd);
+        Blutdrucks = (EditText) findViewById(R.id.Blutdrucks);
+        Blutdruckd = (EditText) findViewById(R.id.Blutdruckd);
+    }
+
+    public void updateText(View view){
+      Textfieldblutdrucks.setText("Ihr systolischer Blutdruck beträgt "+Blutdrucks.getText());
+      Textfieldblutdruckd.setText("Ihr diastolischer Blutdruck beträgt "+Blutdruckd.getText());
+
     }
 }
